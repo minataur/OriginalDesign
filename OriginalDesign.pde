d29221be3af5b495c135a122acbd7de612f41493
mintaur <minac.98sf@gmail.com>
2014-08-22 11:34:51 -0700

    circles and quads

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,22 @@
 void setup()
 {
-
+  size(1000,1000);
+  background(255,0,238);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+  drawCircle();
+  drawRandomShit();
 }
-
+void drawCircle()
+{
+  noStroke();
+  fill(random(0,255),random(0,255),random(0,255));
+  ellipse(random(0,1000),random(0,1000),169,159);
+}
+void drawRandomShit()
+{
+  noStroke();
+  fill(255,115,0,50);
+  quad(random(0,1000),random(0,1000)/*1*/,random(0,1000),random(0,1000)/*2*/,random(0,1000),random(0,1000)/*3*/,random(0,1000),random(0,1000)/*4*/);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,13 +16,26 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-
+  size(1000,1000);
+  background(255,0,238);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+  drawCircle();
+  drawRandomShit();
 }
-
+public void drawCircle()
+{
+  noStroke();
+  fill(random(0,255),random(0,255),random(0,255));
+  ellipse(random(0,1000),random(0,1000),169,159);
+}
+public void drawRandomShit()
+{
+  noStroke();
+  fill(255,115,0,50);
+  quad(random(0,1000),random(0,1000)/*1*/,random(0,1000),random(0,1000)/*2*/,random(0,1000),random(0,1000)/*3*/,random(0,1000),random(0,1000)/*4*/);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
